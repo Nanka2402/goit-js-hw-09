fa8cf64a2aafe00b5213c1a2c30ecc4e71653b91
Nanka2402 <nastia.secr@gmail.com>
2023-12-30 22:13:18 +0200

    task1-2

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,7 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const images = [
   {
     preview:
@@ -78,3 +82,8 @@ const createGallImg = ({ preview, original, description }) => `
 const gallImg = images.map(createGallImg).join('');
 const gallery = document.querySelector('.gallery');
 gallery.insertAdjacentHTML('beforeend', gallImg);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
