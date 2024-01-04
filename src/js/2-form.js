b4497d26643347de429a2b29b800913313ee8f30
Nanka2402 <nastia.secr@gmail.com>
2024-01-04 23:31:00 +0200

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,18 +1,37 @@
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const textarea = form.elements.message;
-const inputForm = form.elements.email;
-const localStorageKey = 'feedback-form-state';
+const email = form.elements.email;
 
-textarea.value = localStorage.getItem(localStorageKey) ?? '';
-inputForm.value = localStorage.getItem(localStorageKey) ?? '';
-form.addEventListener('input', evt => {
-  localStorage.setItem(localStorageKey, evt.target.value);
-});
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormInput);
 
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  console.log(evt.target.elements.message.value);
-  console.log(evt.target.elements.email.value);
-  localStorage.removeItem(localStorageKey);
-  form.reset();
-});
+function onFormInput(evt) {
+  const formImput = {};
+  formImput.email = evt.currentTarget.elements.email.value.trim();
+  formImput.text = evt.currentTarget.elements.message.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formImput));
+}
+
+function savedFormText() {
+  const savedmessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedmessage) {
+    textarea.value = savedmessage.text.trim();
+    email.value = savedmessage.email.trim();
+  }
+}
+savedFormText();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value.trim();
+  const text = event.currentTarget.elements.message.value.trim();
+  if (email === '' || text === '') {
+    alert('Please fill in all the fields!');
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+}
